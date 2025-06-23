f0aae7ee0dfb890845940a1f8b3f0a8852f92763
animmjfan <animmjfan@gmail.com>
2025-06-23 20:15:48 +1000

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,143 @@
-number = 0
-menu_display = 0
+num = 0
+num2 = 0
+ans = 0
+menu = 1
 eq_type = 0
 
 def on_forever():
-    pass
+    basic.clear_screen()
+    # 1st menu section
+    while menu == 1:
+        basic.show_number(num)
+        
+        def on_button_pressed_b():
+            global num
+            num += 1
+        input.on_button_pressed(Button.B, on_button_pressed_b)
+        
+        
+        def on_button_pressed_a():
+            global num
+            num -= 1
+        input.on_button_pressed(Button.A, on_button_pressed_a)
+        
+        
+        def on_pin_pressed_p0():
+            global menu
+            menu = 2
+            basic.clear_screen()
+        input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+        
+    # 2nd menu section
+    while menu == 2:
+        basic.show_number(num2)
+        
+        def on_button_pressed_b2():
+            global num2
+            num2 += 1
+        input.on_button_pressed(Button.B, on_button_pressed_b2)
+        
+        
+        def on_button_pressed_a2():
+            global num2
+            num2 -= 1
+        input.on_button_pressed(Button.A, on_button_pressed_a2)
+        
+        
+        def on_pin_pressed_p02():
+            global menu
+            menu = 3
+            basic.clear_screen()
+        input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p02)
+        
+    # 3rd menu section
+    while menu == 3:
+        basic.show_string("op", 100)
+        
+        def on_button_pressed_a3():
+            global ans, menu
+            ans = num + num2
+            menu = 4
+        input.on_button_pressed(Button.A, on_button_pressed_a3)
+        
+        
+        def on_button_pressed_b3():
+            global ans, menu
+            ans = num - num2
+            menu = 4
+        input.on_button_pressed(Button.B, on_button_pressed_b3)
+        
+        
+        def on_pin_pressed_p03():
+            global ans, menu
+            ans = num * num2
+            menu = 4
+        input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p03)
+        
+        
+        def on_pin_pressed_p1():
+            global ans, menu
+            ans = num / num2
+            menu = 4
+        input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+        
+        
+        def on_pin_pressed_p2():
+            global menu
+            menu = 1
+        input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+        
+    # 4th menu section
+    while menu == 4:
+        basic.clear_screen()
+        basic.show_number(ans)
+        # handle restarting on any key
+        
+        def on_button_pressed_a4():
+            global menu, num2, num
+            menu = 1
+            num = 0
+            num2 = 0
+        input.on_button_pressed(Button.A, on_button_pressed_a4)
+        
+        
+        def on_button_pressed_b4():
+            global menu, num2, num
+            menu = 1
+            num = 0
+            num2 = 0
+        input.on_button_pressed(Button.B, on_button_pressed_b4)
+        
+        
+        def on_button_pressed_ab():
+            global menu, num2, num
+            menu = 1
+            num = 0
+            num2 = 0
+        input.on_button_pressed(Button.AB, on_button_pressed_ab)
+        
+        
+        def on_pin_pressed_p04():
+            global menu, num2, num
+            menu = 1
+            num = 0
+            num2 = 0
+        input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p04)
+        
+        
+        def on_pin_pressed_p12():
+            global menu, num2, num
+            menu = 1
+            num = 0
+            num2 = 0
+        input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p12)
+        
+        
+        def on_pin_pressed_p22():
+            global menu, num2, num
+            menu = 1
+            num = 0
+            num2 = 0
+        input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p22)
+        
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -4,86 +4,109 @@ let ans = 0
 let menu = 1
 let eq_type = 0
 basic.forever(function on_forever() {
-	basic.clearScreen()
-    // 1st menu section
-	while (menu == 1) {
-		basic.showNumber(num)
-        input.onButtonPressed(Button.B, function() {
-            num++
+    basic.clearScreen()
+    //  1st menu section
+    while (menu == 1) {
+        basic.showNumber(num)
+        input.onButtonPressed(Button.B, function on_button_pressed_b() {
+            
+            num += 1
         })
-        input.onButtonPressed(Button.A, function() {
-            num--
+        input.onButtonPressed(Button.A, function on_button_pressed_a() {
+            
+            num -= 1
         })
-        input.onPinPressed(TouchPin.P0, function() {
+        input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+            
             menu = 2
             basic.clearScreen()
         })
     }
-    // 2nd menu section
+    //  2nd menu section
     while (menu == 2) {
         basic.showNumber(num2)
-        input.onButtonPressed(Button.B, function() {
-            num2++
+        input.onButtonPressed(Button.B, function on_button_pressed_b2() {
+            
+            num2 += 1
         })
-        input.onButtonPressed(Button.A, function() {
-            num2--
+        input.onButtonPressed(Button.A, function on_button_pressed_a2() {
+            
+            num2 -= 1
         })
-        input.onPinPressed(TouchPin.P0, function() {
+        input.onPinPressed(TouchPin.P0, function on_pin_pressed_p02() {
+            
             menu = 3
             basic.clearScreen()
         })
     }
-    // 3rd menu section
+    //  3rd menu section
     while (menu == 3) {
         basic.showString("op", 100)
-        input.onButtonPressed(Button.A, function() {
+        input.onButtonPressed(Button.A, function on_button_pressed_a3() {
+            
             ans = num + num2
             menu = 4
         })
-        input.onButtonPressed(Button.B, function() {
+        input.onButtonPressed(Button.B, function on_button_pressed_b3() {
+            
             ans = num - num2
             menu = 4
         })
-        input.onPinPressed(TouchPin.P0, function() {
+        input.onPinPressed(TouchPin.P0, function on_pin_pressed_p03() {
+            
             ans = num * num2
             menu = 4
         })
-        input.onPinPressed(TouchPin.P1, function() {
+        input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+            
             ans = num / num2
             menu = 4
         })
-        input.onPinPressed(TouchPin.P2, function() {
+        input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+            
             menu = 1
         })
     }
-    // 4th menu section
+    //  4th menu section
     while (menu == 4) {
         basic.clearScreen()
         basic.showNumber(ans)
-        // handle restarting on any key
-        input.onButtonPressed(Button.A, function() {
+        //  handle restarting on any key
+        input.onButtonPressed(Button.A, function on_button_pressed_a4() {
+            
             menu = 1
-            num = num2 = 0
+            num = 0
+            num2 = 0
         })
-        input.onButtonPressed(Button.B, function() {
+        input.onButtonPressed(Button.B, function on_button_pressed_b4() {
+            
             menu = 1
-            num = num2 = 0
+            num = 0
+            num2 = 0
         })
-        input.onButtonPressed(Button.AB, function() {
+        input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+            
             menu = 1
-            num = num2 = 0
+            num = 0
+            num2 = 0
         })
-        input.onPinPressed(TouchPin.P0, function() {
+        input.onPinPressed(TouchPin.P0, function on_pin_pressed_p04() {
+            
             menu = 1
-            num = num2 = 0
+            num = 0
+            num2 = 0
         })
-        input.onPinPressed(TouchPin.P1, function() {
+        input.onPinPressed(TouchPin.P1, function on_pin_pressed_p12() {
+            
             menu = 1
-            num = num2 = 0
+            num = 0
+            num2 = 0
         })
-        input.onPinPressed(TouchPin.P2, function() {
+        input.onPinPressed(TouchPin.P2, function on_pin_pressed_p22() {
+            
             menu = 1
-            num = num2 = 0
+            num = 0
+            num2 = 0
         })
     }
 })
